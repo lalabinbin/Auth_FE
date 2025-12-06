446ec5620986f71a801d29b85a66c99fbdfad629
lalabinbin <binhminh2711@gmail.com>
2025-12-06 15:57:51 +0700

    fix: correct import path for Spinner component

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Spinner } from "@/components/ui/Spinner";
+import { Spinner } from "@/components/ui/spinner";
 export function LoginForm({
   handleLogin,
   loading,
